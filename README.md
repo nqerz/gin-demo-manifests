6e3ed2843d22fd320e44945bb3aa63a599c1cea0
znqerz <73216018+znqerz@users.noreply.github.com>
2022-08-02 21:36:08 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# gin-demo-kustomize
+
+demo for kustomize
+
+默认action不允许提交commit，需要到settings->action 更改workflow的读写权限
---
+++ scripts/make-rules/common.mk
@@ -0,0 +1,8 @@
+COMMON_SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+ifeq ($(origin ROOT_DIR),undefined)
+ROOT_DIR := $(abspath $(shell cd $(COMMON_SELF_DIR)/../.. && pwd -P))
+endif
+
+BASE_DIR=$(ROOT_DIR)/base
+ENVS_DIR=$(ROOT_DIR)/envs
